89a04f40359f051a4db4cb0068144f608fc55d52
vinothvk99 <46102262+vinothvk99@users.noreply.github.com>
2018-12-24 15:16:17 +0530

    Create vowelorconstant.c

---
+++ vowelorconstant.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ char input;
+ clrscr();
+ printf("Enter the character to check ");
+ scanf("%c",&input);
+ if (input=='a' || input=='e' || input=='i' || input=='o' || input=='u' || input=='A' || input=='E' || input=='I' || input=='O' || input=='U')
+ {
+  printf("Vowels");
+ }
+ else
+ {
+  printf("Constant");
+ }
+getch();
+}
