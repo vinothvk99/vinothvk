398c69d19861ecdde1377399655c03261af713c7
vinothvk99 <46102262+vinothvk99@users.noreply.github.com>
2019-01-24 12:31:11 +0530

    Update strrev.c

--- strrev.c
+++ strrev.c
@@ -2,17 +2,17 @@
 #include<conio.h>
 void main()
 {
-char string[50],r_string[50];
+char str[50],r_str[50];
 int i=0,j;
 clrscr();
-scanf("%s",string);
-j=strlen(string);
+scanf("%s",str);
+j=strlen(str);
 for(j=j-1;j>=0;j--)
 {
- r_string[i]=string[j];
+ r_str[i]=str[j];
 i++;
 }
-r_string[i]='\0';
-printf("%s",r_string);
+r_str[i]='\0';
+printf("%s",r_str);
 getch();
 }
