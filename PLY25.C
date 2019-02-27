7b9fb43c9d055b36bccfa25d24627314aa9a0abf
vinothvk99 <46102262+vinothvk99@users.noreply.github.com>
2019-02-27 21:36:25 +0530

    Create PLY25.C

---
+++ PLY25.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main()
+{
+  int NUM,i,len,k=0,z[10],m,j,t;
+  char a[10][10];
+  scanf("%d",&NUM);
+  for(i=0;i<NUM;i++)
+  {
+      scanf("%s",&a[i]);   
+  }
+   for(i=0;i<NUM;i++)
+  {   
+len=strlen(a[i]);   
+ z[k]=len;
+      k++;
+  }
+   for(i=0;i<NUM;i++)
+  {
+  for(j=i+1;j<NUM;j++)
+   {
+       if(z[i]>z[j])
+       {
+           t=z[i];
+           z[i]=z[j];
+           z[j]=t;   
+       }    
+   }
+  }
+  for(i=0;i<NUM;i++)
+  {    
+      for(j=0;j<NUM;j++)
+      {
+          
+          if(z[i]==strlen(a[j]))
+          printf("%s\n",a[j]);
+      }
+  }
+return 0;
+}
